bb0fa605c054407476ce93533e6a7afc23f6e710
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-12-01 23:02:41 +0900

    init: 프로젝트를 생성하다
    - Java 버전: 17
    - 빌드: Gradle

---
+++ src/main/java/com/programmers/kwonjoosung/BootCampRatingNet/BootCampRatingNetApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.kwonjoosung.BootCampRatingNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampRatingNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampRatingNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/kwonjoosung/BootCampRatingNet/BootCampRatingNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.kwonjoosung.BootCampRatingNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampRatingNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
