6794adb09de81dd39c4ad0f5c981e2d16c688c39
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-12-09 08:04:52 +0900

    refactor: 환경설정 수정
    - application.yml로 수정
    - schema.sql 수정
    - logback.xml 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,14 @@
+# create database boot_camp_rating_net;
+create database bootcamp_rating_net;
+USE bootcamp_rating_net;
+
+
 CREATE TABLE bootcamp
 (
     camp_id     varchar(255)  NOT NULL PRIMARY KEY,
     name        varchar(30)   NOT NULL,
-    location    varchar(50)   NOT NULL,
-    description varchar(1000) NOT NULL
+    description varchar(1000) NOT NULL,
+    image       varchar(255)  NOT NULL
 );
 
 CREATE TABLE user
@@ -13,20 +18,18 @@ CREATE TABLE user
     password  varchar(20)  NOT NULL,
     email     varchar(30)  NOT NULL,
     phone     varchar(13)  NOT NULL UNIQUE,
-    address   varchar(50)  NOT NULL,
     bootcamp  varchar(30)  NULL
 );
 
 CREATE TABLE camp_review
 (
     review_id      varchar(255)  NOT NULL PRIMARY KEY,
-    camp_name      varchar(255)  NOT NULL,
-    user_nick_name varchar(255)  NOT NULL,
+    user_id        varchar(255)  NOT NULL,
+    camp_id        varchar(255)  NOT NULL,
     title          varchar(30)   NOT NULL,
-    comment        varchar(300)  NOT NULL,
+    content        varchar(300)  NOT NULL,
     rating         DECIMAL(5, 2) NOT NULL,
     created_at     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_camp FOREIGN KEY (camp_name) REFERENCES bootcamp (name) on delete cascade on update cascade,
-    CONSTRAINT fk_user FOREIGN KEY (user_nick_name) REFERENCES user (nick_name) on delete cascade on update cascade
+    CONSTRAINT fk_camp FOREIGN KEY (camp_id) REFERENCES bootcamp (camp_id) on delete cascade on update cascade,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user (user_id) on delete cascade on update cascade
 );
